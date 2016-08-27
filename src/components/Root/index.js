20bf81f142bb88eb55493bc2706c853b903f2985
Tyler Nieman <tyler@tsnieman.net>
2016-08-26 17:28:33 -0700

    add a couple '.default' to various uses of 'require' for good measure

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,7 +1,7 @@
 // Use DefinePlugin (Webpack) together with Uglify
 // to strip the dev branch in prod build.
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod'); // eslint-disable-line global-require
+  module.exports = require('./Root.prod').default; // eslint-disable-line global-require
 } else {
-  module.exports = require('./Root.dev'); // eslint-disable-line global-require
+  module.exports = require('./Root.dev').default; // eslint-disable-line global-require
 }
