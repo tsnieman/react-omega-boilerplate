7a251da4b96a054df3e506e33fe1839e791ca819
tyler <tyler@tsnieman.net>
2016-12-30 15:59:23 -0800

    upgrade normalizr to 3.0.0

--- src/constants/entities.js
+++ src/constants/entities.js
@@ -1,15 +1,21 @@
-import { Schema, arrayOf } from 'normalizr';
+import { schema } from 'normalizr';
 
 export const ACTIONS = {
   FETCH_ENTITY: 'FETCH_ENTITY',
   SET_ENTITIES: 'SET_ENTITIES',
 };
 
-const userSchema = new Schema('users', {
-  idAttribute: (user) => user.login.toLowerCase(),
-});
+const userSchema = new schema.Entity('users',
+  // definition
+  {},
+
+  // options
+  {
+    idAttribute: (user) => user.login.toLowerCase(),
+  },
+);
 
 export const SCHEMAS = {
   USER: userSchema,
-  USER_ARRAY: arrayOf(userSchema),
+  USER_ARRAY: schema.Array[userSchema],
 };
